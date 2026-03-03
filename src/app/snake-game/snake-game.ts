eb285ad38051a233fba6e8c00697956ecefe291a
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-03-03 21:09:51 +0000

    Snake Game!

---
+++ src/app/snake-game/snake-game.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snake-game',
+  imports: [],
+  templateUrl: './snake-game.html',
+  styleUrl: './snake-game.css',
+})
+export class SnakeGame {}
