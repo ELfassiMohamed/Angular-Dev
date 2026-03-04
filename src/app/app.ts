95ecaa0900528ed0187094ee0c7f51a6b11ae68f
ELfassiMohamed <elfassimohamed2001@gmail.com>
2026-03-04 22:54:26 +0000

    Snake Game work but uhh...!

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { Component, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App {
+//   protected readonly title = signal('jarvis_bot');
+// }
+import { Component } from '@angular/core';
+import { SnakeGame } from './snake-game/snake-game';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SnakeGame],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('jarvis_bot');
-}
+export class App {}
